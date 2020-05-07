e9c2f2159dc1135fa2bde22c7847eeda0621b16a
deobarro <deobarro@gmail.com>
2020-05-07 18:03:41 +0200

    Segunda escenario con grabador de eventos

--- test/redireccionInsertarDatos/irAInsertarDatos.js
+++ test/redireccionInsertarDatos/irAInsertarDatos.js
@@ -1,11 +1,16 @@
 describe("irAInsertarDatos", function() {
     it("Existencia de una tabla es correcto", function() {
     
-    ST.element('img[id="imagen1"]').click();
+    /*ST.element('img[id="imagen1"]').click();
     ST.element('td[name="Flexiones"]')
     .get("innerHTML")
     .and(function(){
         expect(this.future.data.innerHTML).toBe("Flexiones");
-        });
+        });*/
+    ST.play([
+        { type: "tap", target: "@imagen1", x: 115, y: 91 },
+        { type: "tap", target: "//body/section[1]/div[1]/table[1]/thead[1]/tr[1]/td[1]", x: 30, y: 9 }
+    ]);
+        expect("//body/section[1]/div[1]/table[1]/thead[1]/tr[1]/td[1]").toBeDefined();
     });
 });
