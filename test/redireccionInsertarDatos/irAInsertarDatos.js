85bfb42339c76fac786715f2fb33cbc1a9dddb51
deobarro <deobarro@gmail.com>
2020-05-08 09:54:29 +0200

    Nueva modificación

--- test/redireccionInsertarDatos/irAInsertarDatos.js
+++ test/redireccionInsertarDatos/irAInsertarDatos.js
@@ -4,6 +4,5 @@ describe("irAInsertarDatos", function() {
         { type: "tap", target: "@imagen1", x: 115, y: 91 },
         { type: "tap", target: "//body/section[1]/div[1]/table[1]/thead[1]/tr[1]/td[1]", x: 30, y: 9 }
     ]);
-        expect("//body/section[1]/div[1]/table[1]/thead[1]/tr[1]/td[1]").toBeDefined();
     });
 });
